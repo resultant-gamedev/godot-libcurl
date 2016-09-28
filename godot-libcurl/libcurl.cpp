24782cf3174bd95555b2c84a189d563f650ee888
ArcFutahito <futahito.arc@gmail.com>
2016-09-28 14:46:19 +0300

    more header stuff

--- godot-libcurl/libcurl.cpp
+++ godot-libcurl/libcurl.cpp
@@ -36,11 +36,11 @@ CURLcode Libcurl::easy_pause(handle, bitmask) {
    
 }
 
-void Libcurl::easy_perform() {
+void Libcurl::easy_perform(handle) {
 
 }
 
-void Libcurl::easy_recv() {
+CURLcode Libcurl::easy_recv(curl, buffer, buflen, n) {
 
 }
 
@@ -52,7 +52,7 @@ void Libcurl::easy_send() {
 
 }
 
-void Libcurl::easy_setopt() {
+CURLcode Libcurl::easy_setopt(handle, option, parameter) {
 
 }
 
@@ -91,11 +91,80 @@ void Libcurl::getdate() {
 
 }
 
+/*
+#################################
+     Multi interface
+#################################
+*/
+
+void Libcurl::multi_add_handle() {
+
+}
+
+void Libcurl::multi_assign() {
+
+}
+
+void Libcurl::multi_cleanup() {
+
+}
+
+void Libcurl::multi_fdset() {
+
+}
 
 
+void Libcurl::multi_info_read() {
+
+}
+
+void Libcurl::multi_init() {
+
+}
+
+void Libcurl::multi_perform() {
+
+}
+
+void Libcurl::multi_remove_handle() {
+
+}
+
+void Libcurl::multi_setopt() {
+
+}
+
+void Libcurl::multi_socket_action() {
+
+}
+
+void Libcurl::multi_sterror() {
+
+}
+
+void Libcurl::multi_slist_append() {
+
+}
+
+void Libcurl::multi_slist_free_all() {
+
+}
+
+void Libcurl::version() {
+
+}
+
+void Libcurl::version_info() {
+
+}
+
+/*
+##################################
+            Bindings
+##################################
+*/
 
 void Libcurl::_bind_methods() {
-
    ObjectTypeDB::bind_method("easy_init",&:Libcurl::easy_init);
    ObjectTypeDB::bind_method("easy_cleanup",&:Libcurl::easy_cleanup);
    ObjectTypeDB::bind_method("easy_duphandle",&:Libcurl::easy_duphandle);
@@ -110,11 +179,25 @@ void Libcurl::_bind_methods() {
    ObjectTypeDB::bind_method("easy_sterror",&:Libcurl::easy_sterror);
    ObjectTypeDB::bind_method("easy_unescape",&:Libcurl::easy_unescape);
 
-
    ObjectTypeDB::bind_method("formadd",&:Libcurl::formadd);
    ObjectTypeDB::bind_method("formfree",&:Libcurl::formfree);
    ObjectTypeDB::bind_method("formget",&:Libcurl::formget);
    ObjectTypeDB::bind_method("free",&:Libcurl::free);
    ObjectTypeDB::bind_method("getdate",&:Libcurl::getdate);
 
+   ObjectTypeDB::bind_method("multi_add_handle",&:Libcurl::multi_add_handle);
+   ObjectTypeDB::bind_method("multi_assign",&:Libcurl::multi_assign);
+   ObjectTypeDB::bind_method("multi_cleanup",&:Libcurl::multi_cleanup);
+   ObjectTypeDB::bind_method("multi_fdset",&:Libcurl::multi_fdset);
+   ObjectTypeDB::bind_method("multi_info_read",&:Libcurl::multi_info_read);
+   ObjectTypeDB::bind_method("multi_init",&:Libcurl::multi_init);
+   ObjectTypeDB::bind_method("multi_perform",&:Libcurl::multi_perform);
+   ObjectTypeDB::bind_method("multi_remove_handle",&:Libcurl::multi_remove_handle);
+   ObjectTypeDB::bind_method("multi_setopt",&:Libcurl::multi_setopt);
+   ObjectTypeDB::bind_method("multi_socket_action",&:Libcurl::multi_socket_action);
+   ObjectTypeDB::bind_method("multi_sterror",&:Libcurl::multi_sterror);
+   ObjectTypeDB::bind_method("multi_slist_append",&:Libcurl::multi_slist_append);
+   ObjectTypeDB::bind_method("multi_slist_free_all",&:Libcurl::multi_slist_free_all);
+   ObjectTypeDB::bind_method("version",&:Libcurl::version);
+   ObjectTypeDB::bind_method("version_info",&:Libcurl::multi_version_info);
 }
--- godot-libcurl/libcurl.h
+++ godot-libcurl/libcurl.h
@@ -9,6 +9,7 @@ Custom data type: CURL
 
 #include "reference.h"
 
+
 class Libcurl : public Reference {
    OBJ_TYPE(Libcurl,Reference);
    
@@ -18,33 +19,72 @@ protected:
    static void _bind_methods();
 
 public:
-   /*
-   #############################
-          Easy interface
-   #############################
-   */
-   void easy_cleanup(CURL handle);
-   CURL easy_duphandle();
-   char easy_escape(CURL curl, auto data, int length);
+   void easy_cleanup(CURL * handle );
+   CURL *easy_duphandle(CURL *handle );
+   char *easy_escape( CURL * curl , const char * string , int length ); 
    void easy_getinfo();
-   void easy_init();
+   CURL *easy_init( );
    void easy_pause(CURL handle, int bitmask);
-   void easy_perform();
-   void easy_recv();
-   void easy_reset();
-   void easy_send();
-   void easy_setopt();
-   void easy_sterror();
-   void easy_unescape();
+   CURLcode easy_perform(CURL *handle);
+   CURLcode easy_recv(CURL *curl, void *buffer, size_t buflen, size_t *n);
+   void easy_reset(CURL *handle );
+   CURLcode easy_send( CURL * curl , const void * buffer , size_t buflen , size_t * n );
+   CURLcode easy_setopt(CURL *handle, CURLoption option, parameter); 
+   const char *easy_strerror(CURLcode errornum);\
+   char *easy_unescape( CURL * curl , const char * url , int inlength , int * outlength ); 
+
 /*
 ################################
      End of easy interface
 ################################
 */
 
+   void formfree(struct curl_httppost * form);
+   int formget(struct curl_httppost * form, void *userp, curl_formget_callback append );
+   void free( char * ptr );
+   time_t getdate(char * datestring , time_t *now );
+   void global_cleanup(void);
+   CURLcode curl_global_init(long flags);
+   CURLcode curl_global_init_mem(long  flags,
+                                 curl_malloc_callback m,
+                                 curl_free_callback f,
+                                 curl_realloc_callback r,
+                                 curl_strdup_callback s,
+                                 curl_calloc_callback c );
+   CURLMcode multi_add_handle(CURLM *multi_handle, CURL *easy_handle);
+   CURLMcode multi_assign(CURLM *multi_handle, curl_socket_t sockfd, void *sockptr);  
+   CURLMcode multi_cleanup( CURLM *multi_handle );
+   CURLMcode multi_fdset(CURLM *multi_handle,
+                         fd_set *read_fd_set,
+                         fd_set *write_fd_set,
+                         fd_set *exc_fd_set,
+                         int *max_fd);
+   CURLMsg *multi_info_read( CURLM *multi_handle, int *msgs_in_queue); 
+   CURLM *multi_init();
+   CURLMcode multi_perform(CURLM *multi_handle, int *running_handles); 
+   CURLMcode multi_remove_handle(CURLM *multi_handle, CURL *easy_handle);
+   CURLMcode multi_setopt(CURLM * multi_handle, CURLMoption option, param); 
+   CURLMcode multi_socket_action(CURLM * multi_handle,
+                                 curl_socket_t sockfd,
+                                 int ev_bitmask,
+                                 int *running_handles);
+   const char *multi_strerror(CURLMcode  errornum);
+   CURLMcode multi_timeout(CURLM *multi_handle, long *timeout);
+   CURLMcode multi_wait(CURLM *multi_handle,
+                        struct curl_waitfd extra_fds[],
+                        unsigned int extra_nfds,
+                        int timeout_ms,
+                        int *numfds);
+   CURLSHcode share_cleanup(CURLSH * share_handle);
+   CURLSH *share_init();
+   CURLSHcode share_setopt(CURLSH *share, CURLSHoption option, parameter); 
+   const char *share_strerror(CURLSHcode  errornum);
+   struct slist *slist_append(struct curl_slist * list, const char * string);
+   void slist_free_all(struct curl_slist * list);
+   char *version();
+   curl_version_info_data *version_info(CURLversion type);
 
    Libcurl();
-
 };
 
 #endif
